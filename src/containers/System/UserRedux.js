8dfe63577786cabd742aceaddee06e989f7d73f4
DauThiHoa <19130075@st.hcmuaf.edu.vn>
2023-04-30 15:59:54 +0700

    UpdateLoadImage

--- src/containers/System/UserRedux.js
+++ src/containers/System/UserRedux.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux'; 
-import { LANGUAGES , CRUD_ACTIONS} from '../../utils';
+import { LANGUAGES , CRUD_ACTIONS, CommonUtils} from '../../utils';
 import * as actions from "../../store/actions";
 import './UserRedux.scss';
 import { forIn } from 'lodash';
@@ -110,7 +110,7 @@ class UserRedux extends Component {
                 position: arrPositions && arrPositions.length > 0 ? arrPositions[0].key :'',
                 avatar: '',   
                 action: CRUD_ACTIONS.CREATE ,
-                
+                previewImgURL:''
             })
         } 
 
@@ -120,16 +120,18 @@ class UserRedux extends Component {
 
     // HAM LOAD HINH ANH 
 
-    handleOnChangeImage = (event) => {
+    handleOnChangeImage = async (event) => {
 
         let data = event.target.files;
         let file = data[0];
         // TAO DUONG LINK ANH
         if ( file ){
+            let base64 = await CommonUtils.getBase64(file); 
+
             let objectUrl = URL.createObjectURL(file);
             this.setState ({
                 previewImgURL: objectUrl,
-                avatar: file
+                avatar: base64
             })
         }
  
@@ -163,6 +165,7 @@ class UserRedux extends Component {
             gender: this.state.gender,
             roleId: this.state.role,
             positionId: this.state.position,
+            avatar: this.state.avatar
         })
     }
     if ( action === CRUD_ACTIONS.EDIT){
@@ -178,7 +181,7 @@ class UserRedux extends Component {
                 gender: this.state.gender,
                 roleId: this.state.role,
                 positionId: this.state.position,
-                // avatar: this.state.avatar,
+                avatar: this.state.avatar,
 
             })
     }
@@ -219,6 +222,14 @@ class UserRedux extends Component {
     }
 
     handleEditUserFromParent = (user) =>{
+
+        // LAY ANH DUOI DATABASE
+        let imageBase64 = '' ;
+        if (user.image){ 
+            imageBase64 = new Buffer (user.image, 'base64').toString('binary');
+            // setImage (imageBase64)
+        } 
+
         console.log ('hoi dan it check from : ' , user)
 
         this.setState ({ 
@@ -232,9 +243,11 @@ class UserRedux extends Component {
             role: user.roleId,
             position: user.positionId,
             avatar: '',    
+            previewImgURL: imageBase64,
             action: CRUD_ACTIONS.EDIT,
             userEditId: user.id,
-        })
+        } 
+        )
 
     }
 // HIEN THI TRANG QUAN LY GOI
