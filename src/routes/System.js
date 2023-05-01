da5261f5ac9c2727fa3a7e2006bfdb42e57c30b7
DauThiHoa <19130075@st.hcmuaf.edu.vn>
2023-05-01 14:31:53 +0700

    ThongTinChiTietBacSi

--- src/routes/System.js
+++ src/routes/System.js
@@ -4,6 +4,7 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import UserManage from '../containers/System/UserManage';
 import UserRedux from '../containers/System/UserRedux'; 
 import Header from '../containers/Header/Header';
+import ManageDoctor from '../containers/System/Admin/ManageDoctor';
 
 class System extends Component {
     render() {
@@ -17,9 +18,10 @@ class System extends Component {
 
             <div className="system-container">
                 <div className="system-list">
-                    <Switch>
+                    <Switch> 
                         <Route path="/system/user-manage" component={UserManage} />
                         <Route path="/system/user-redux" component={UserRedux} />
+                        <Route path="/system/manage-doctor" component={ManageDoctor} />
                         <Route component={() => { return (<Redirect to={systemMenuPath} />) }} />
                     </Switch>
                 </div>
